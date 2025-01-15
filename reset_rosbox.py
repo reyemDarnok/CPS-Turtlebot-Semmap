6203f22c881a5d15562b7a943a8d7936a97c1279
Ben Konrad Meyer <ben_konrad.meyer@smail.th-koeln.de>
2025-01-15 15:32:25 +0100

    docker definitions

---
+++ reset_rosbox.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+import docker
+
+def main():
+    image_name = "rosbox"
+    container_name = "roscontainer"
+    dclient = docker.from_env()
+    cwd = Path.cwd().absolute()
+    dclient.images.build(path=(cwd / "docker").absolute().__str__(), tag=image_name, pull=True)
+    try:
+        dclient.containers.get(container_name).remove(force=True)
+    except docker.errors.NotFound:
+        pass
+    dclient.containers.run(image=image_name,
+                           detach=True,
+                           name=container_name,
+                           volumes=[
+                               "/tmp/.X11-unix:/tmp/.X11-unix",
+                                f"{str(cwd)}workspace:/home/rosuser/workspace"
+                           ],
+                           environment={"DISPLAY": os.environ.get("DISPLAY")},
+                           )
+
+
+if __name__ == "__main__":
+    main()
