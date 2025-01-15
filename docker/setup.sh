22add6bfdd6069b9ce5c40a2888e69c749072de3
Ben Konrad Meyer <ben_konrad.meyer@smail.th-koeln.de>
2025-01-15 18:11:19 +0100

    streamline setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 echo "$@"
 rosdep update
+pushd workspace || exit 1
+./update_ros.sh
+source install/setup.sh
+popd || exit 1
 touch ~/initial_install_complete
 # execute CMD
 exec "$@"
