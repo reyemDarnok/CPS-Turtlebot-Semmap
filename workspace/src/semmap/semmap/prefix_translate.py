dd0548708a9e03d30c5f4354678bc3d32854c215
Ben Konrad Meyer <ben_konrad.meyer@smail.th-koeln.de>
2025-03-08 10:45:56 +0100

    fix prefix removal

--- workspace/src/semmap/semmap/prefix_translate.py
+++ workspace/src/semmap/semmap/prefix_translate.py
@@ -23,23 +23,23 @@ class PrefixTranslatorNode(Node):
         robot_output_topics = [topic for topic in topics if topic.startswith(prefix) and topic[len(prefix):] not in robot_input_topics]
         self.get_logger().info(f"{robot_output_topics=}")
         for topic in robot_output_topics:
-            topic_info_process = run(["ros2", "topic", "info", prefix + topic], capture_output=True, text=True)
+            topic_info_process = run(["ros2", "topic", "info", topic], capture_output=True, text=True)
             self.get_logger().info(topic_info_process.stdout)
             info = topic_info_process.stdout.splitlines()[0]
             message_type = info[len("Type: "):]
             t = __import__(message_type.replace("/", "."))
-            pub = self.create_publisher(t, topic, 10)
+            pub = self.create_publisher(t, topic[len(prefix):], 10)
             self.translate_publishers.append(pub)
             def translator(msg):
                 pub.publish(msg)
-            self.create_subscription(t, prefix + topic, translator, 10)
+            self.create_subscription(t, topic, translator, 10)
         for topic in robot_input_topics:
             topic_info_process = run(["ros2", "topic", "info", topic], capture_output=True, text=True)
             self.get_logger().info(topic_info_process.stdout)
             info = topic_info_process.stdout.splitlines()[0]
             message_type = info[len("Type: "):]
             t = __import__(message_type.replace("/", "."))
-            pub = self.create_publisher(t, prefix + topic, 10)
+            pub = self.create_publisher(t, topic[len(prefix):], 10)
             self.translate_publishers.append(pub)
             def translator(msg):
                 pub.publish(msg)
