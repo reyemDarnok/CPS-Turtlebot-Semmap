990de80535bacc2817644714072878dedf1cd4a9
Ben Konrad Meyer <ben_konrad.meyer@smail.th-koeln.de>
2025-03-13 07:35:22 +0100

    change object detection model

--- workspace/src/semmap/semmap/object_detection.py
+++ workspace/src/semmap/semmap/object_detection.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 
+from inference import get_model
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -17,11 +18,11 @@ class ObjectDetectionNode(Node):
             Image, f'{prefix}/oakd/rgb/preview/image_raw', self.image_callback, 10
         )
         self.depth_sub = self.create_subscription(
-            Image, f'{prefix}/oakd/rgb/preview/depth', self.depth_callback, 10
+            Image, f'{prefix}/oakd/rgb/preview/image_ra2/compressedDepth', self.depth_callback, 10
         )
         self.object_pub = self.create_publisher(Object, '/detected_objects', 10)
 
-        self.model = YOLO("yolov8s.pt")
+        self.model = get_model(model_id="furniture-npgea")#YOLO("yolov8s.pt")
         self.horizontal_fov = 68#81  #check if its correct values of our turtlebot!!!
         self.vertical_fov = 68#52 #check if its correct values of our turtlebot!!!
         self.depth_data = None
